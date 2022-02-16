1b777d6b69af9244eed6ef81036ff3dfe528b39e
Brett Hayes <bretthayes@live.ca>
2022-02-15 23:12:54 -0500

    use cases page and dependant components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,8 @@ export { Layout } from './Layout'
 export { ContentSection } from './ContentSection'
 export { Jumbotron, COLORS } from './Jumbotron'
 export { RequestDemoForm } from './RequestDemoForm'
+export { CustomerLogosSectionAnimated } from './CustomerLogosSectionAnimated'
+export { TrySourcegraph } from './TrySourcegraph'
 
 // Page Specific
 export { CaseStudyJumbotron, CaseStudyLayout, InContentBlockquote, InContentImage, MediaQuote } from './CaseStudies'
