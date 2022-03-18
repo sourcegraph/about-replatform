b8d828ec34eac71a2908258a29f58fd26f30bda0
Kathleen Juell <kathleenjuell@gmail.com>
2022-03-18 13:36:44 -0400

    Remove scripts from Layout and update contact pages

--- src/hooks/hubSpot.ts
+++ src/hooks/hubSpot.ts
@@ -68,31 +68,43 @@ function createHubSpotForm({ portalId, formId, targetId, onFormSubmit, onFormRea
         .reduce((key, string) => Object.assign(key, { [string.split('=')[0].trim()]: string.split('=')[1] }), {})
     const anonymousId = getAllCookies.sourcegraphAnonymousUid
     const firstSourceURL = getAllCookies.sourcegraphSourceUrl
-    ;(window as Window).hbspt?.forms.create({
-        portalId,
-        formId,
-        target: `#${targetId}`,
-        onFormSubmit,
-        onFormReady: (form: HubSpotForm) => {
-            if (form) {
-                // We want to populate hidden fields in the form with values stored in cookies when the form loads.
-                const anonymousIdInput = form[0].querySelector('input[name="anonymous_user_id"]') as HTMLInputElement
-                if (anonymousIdInput && anonymousIdInput.value === '') {
-                    // Populate the hidden anonymous_user_id form field with the value from the sourcegraphAnonymousUid cookie.
-                    anonymousIdInput.value = anonymousId || ''
-                }
+    const script = document.querySelector('script[src="//js.hsforms.net/forms/v2.js"')
+    script?.addEventListener('load', () => {
+        ;(window as Window).hbspt?.forms.create({
+            portalId,
+            formId,
+            target: `#${targetId}`,
+            onFormSubmit,
+            onFormReady: (form: HubSpotForm) => {
+                if (form) {
+                    // We want to populate hidden fields in the form with values stored in cookies when the form loads.
+                    const anonymousIdInput = form[0].querySelector(
+                        'input[name="anonymous_user_id"]'
+                    ) as HTMLInputElement
+                    if (anonymousIdInput && anonymousIdInput.value === '') {
+                        // Populate the hidden anonymous_user_id form field with the value from the sourcegraphAnonymousUid cookie.
+                        anonymousIdInput.value = anonymousId || ''
+                    }
 
-                const firstSourceURLInput = form[0].querySelector('input[name="first_source_url"]') as HTMLInputElement
-                const emailInput = form[0].querySelector('input[name="email"]') as HTMLInputElement
-                if (firstSourceURLInput && firstSourceURLInput.value === '' && emailInput && emailInput.value === '') {
-                    // Populate the hidden first_source_url form field with the value from the sourcegraphSourceUrl cookie.
-                    firstSourceURLInput.value = firstSourceURL || ''
+                    const firstSourceURLInput = form[0].querySelector(
+                        'input[name="first_source_url"]'
+                    ) as HTMLInputElement
+                    const emailInput = form[0].querySelector('input[name="email"]') as HTMLInputElement
+                    if (
+                        firstSourceURLInput &&
+                        firstSourceURLInput.value === '' &&
+                        emailInput &&
+                        emailInput.value === ''
+                    ) {
+                        // Populate the hidden first_source_url form field with the value from the sourcegraphSourceUrl cookie.
+                        firstSourceURLInput.value = firstSourceURL || ''
+                    }
                 }
-            }
-            if (onFormReady) {
-                onFormReady(form[0])
-            }
-        },
+                if (onFormReady) {
+                    onFormReady(form[0])
+                }
+            },
+        })
     })
 }
 
