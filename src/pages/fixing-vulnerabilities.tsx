df2b3eb408d45083bf4e789ecb5304250fca3648
Brett Hayes <bretthayes@live.ca>
2022-03-23 11:21:13 -0400

    feedback

--- src/pages/fixing-vulnerabilities.tsx
+++ src/pages/fixing-vulnerabilities.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 
 import { Layout, FormLegal } from '@components'
 import { useHubSpot } from '@hooks'
