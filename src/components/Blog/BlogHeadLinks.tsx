dc264614ced8cb0b4cb407ba9f0af421dab7a56f
Kathleen Juell <kathleenjuell@gmail.com>
2022-04-06 13:20:57 -0400

    Replace bullets

--- src/components/Blog/BlogHeadLinks.tsx
+++ src/components/Blog/BlogHeadLinks.tsx
@@ -9,9 +9,9 @@ import { BLOG_TYPE_TO_INFO } from './postTypes'
 export const BlogHeadLinks: FunctionComponent = () => (
     <p>
         <span className="font-weight-bold pr-2">Past liveblogs:</span>
-        <Link href={BLOG_TYPE_TO_INFO[BlogType.StrangeLoop].baseUrl}>Strange Loop</Link> •{' '}
-        <Link href={BLOG_TYPE_TO_INFO[BlogType.GopherCon].baseUrl}>GopherCon and dotGo</Link> •{' '}
-        <Link href={BLOG_TYPE_TO_INFO[BlogType.GraphQLSummit].baseUrl}>GraphQL Summit</Link> •{' '}
+        <Link href={BLOG_TYPE_TO_INFO[BlogType.StrangeLoop].baseUrl}>Strange Loop</Link> &bull;{' '}
+        <Link href={BLOG_TYPE_TO_INFO[BlogType.GopherCon].baseUrl}>GopherCon and dotGo</Link> &bull;{' '}
+        <Link href={BLOG_TYPE_TO_INFO[BlogType.GraphQLSummit].baseUrl}>GraphQL Summit</Link> &bull;{' '}
         <Link href={BLOG_TYPE_TO_INFO[BlogType.GitHubUniverse].baseUrl}>GitHub Universe</Link>
     </p>
 )
