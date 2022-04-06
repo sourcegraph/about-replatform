bd1e4f262bbf1efbc1891dbf96972a8fd8ff6c77
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-04-06 11:08:57 -0600

    CaseStudyCard hyperlink style update

--- src/components/CaseStudies/CaseStudyCard.tsx
+++ src/components/CaseStudies/CaseStudyCard.tsx
@@ -1,7 +1,5 @@
 import { FunctionComponent } from 'react'
 
-import ArrowRightIcon from 'mdi-react/ArrowRightIcon'
-import ExternalLinkIcon from 'mdi-react/ExternalLinkIcon'
 import Link from 'next/link'
 
 import styles from './CaseStudyCard.module.scss'
@@ -17,20 +15,20 @@ export const CaseStudyCard: FunctionComponent<Props> = props => (
     <div className="card flex-grow-1">
         <div className="card-body">
             <img className={`${styles.logo} mb-2`} src={props.logo} alt={`${props.name} logo`} />
-
-            {props.url.includes('http') ? (
-                <a href={props.url} className="card-link" target="_blank" rel="nofollow noopener noreferrer">
-                    <p className="card-text">{props.title}</p>
-                    <ExternalLinkIcon className="icon-inline ml-1" />
-                </a>
-            ) : (
-                <Link href={props.url} passHref={true}>
-                    <a href="#none" className="card-link">
-                        <p className="card-text">{props.title}</p>
-                        <ArrowRightIcon className="icon-inline ml-1" />
-                    </a>
-                </Link>
-            )}
+            <p className="card-text">
+                {props.title}{' '}
+                <span>
+                    {props.url.includes('http') ? (
+                        <a href={props.url} className="card-link" target="_blank" rel="nofollow noopener noreferrer">
+                            Learn more.
+                        </a>
+                    ) : (
+                        <Link href={props.url} passHref={true}>
+                            <a href="#none" className="card-link">Learn more.</a>
+                        </Link>
+                    )}
+                </span>
+            </p>
         </div>
     </div>
 )
--- src/pages/case-studies/index.tsx
+++ src/pages/case-studies/index.tsx
@@ -12,69 +12,75 @@ interface CaseStudies {
 // TODO: This data will eventually live in our CMS
 const CASESTUDIES: CaseStudies[] = [
     {
+        name: 'Nutanix',
+        logo: '/external-logos/nutanix-logo.svg',
+        title: 'Nutanix fixed Log4j quickly and confidently with Sourcegraph.',
+        url: '/case-studies/nutanix-fixed-log4j-with-sourcegraph',
+    },
+    {
         name: 'FactSet',
         logo: '/external-logos/factset-logo.svg',
-        title: 'FactSet migrates from Perforce to GitHub',
+        title: 'FactSet migrates from Perforce to GitHub.',
         url: '/case-studies/factset-migrates-from-perforce-to-github',
     },
     {
         name: 'Cloudflare',
         logo: '/external-logos/cloudflare-color-logo.svg',
-        title: 'Cloudflare accelerates debugging and improves security',
+        title: 'Cloudflare accelerates debugging and improves security.',
         url: '/case-studies/cloudflare-accelerates-debugging-and-improves-security',
     },
     {
         name: 'Indeed',
         logo: '/external-logos/indeed-logo.svg',
-        title: 'Indeed keeps code up to date and accelerates development velocity',
+        title: 'Indeed keeps code up to date and accelerates development velocity.',
         url: '/case-studies/indeed-accelerates-development-velocity',
     },
     {
         name: 'Workiva',
         logo: '/external-logos/workiva-vector-logo.svg',
-        title: 'Workiva reduces the time it takes to make large-scale code changes by 80%',
+        title: 'Workiva reduces the time it takes to make large-scale code changes by 80%.',
         url: '/case-studies/workiva-automates-large-scale-code-changes',
     },
     {
         name: 'CERN',
         logo: '/external-logos/cern-name-logo.svg',
-        title: 'Sourcegraph empowers CERN to tackle code reuse and code changes in mission-critical applications',
+        title: 'Sourcegraph empowers CERN to tackle code reuse and code changes in mission-critical applications.',
         url: '/case-studies/cern-reduces-technical-debt',
     },
     {
         name: 'Criteo',
         logo: '/external-logos/criteo-logo.svg',
-        title: 'Criteo uses Sourcegraph universal code search to tackle Big Code',
+        title: 'Criteo uses Sourcegraph universal code search to tackle Big Code.',
         url: '/case-studies/criteo-tackles-big-code',
     },
     {
         name: 'F5',
         logo: '/external-logos/f5-logo.svg',
-        title: 'F5 streamlines collaboration for global and distributed software teams',
+        title: 'F5 streamlines collaboration for global and distributed software teams.',
         url: '/case-studies/f5-streamlines-collaboration-globally',
     },
     {
         name: 'Convoy',
         logo: '/external-logos/convoy-logo.svg',
-        title: 'Convoy improves developer onboarding',
+        title: 'Convoy improves developer onboarding.',
         url: '/case-studies/convoy-improved-on-boarding',
     },
     {
         name: 'Lyft',
         logo: '/external-logos/lyft-logo.svg',
-        title: 'Lyft ensures production stability during monolith to microservices transition',
+        title: 'Lyft ensures production stability during monolith to microservices transition.',
         url: '/case-studies/lyft-monolith-to-microservices',
     },
     {
         name: 'Quantcast',
         logo: '/external-logos/quantcast-logo.svg',
-        title: 'Quantcast accelerates large scale refactoring',
+        title: 'Quantcast accelerates large scale refactoring.',
         url: '/case-studies/quantcast-large-scale-refactoring',
     },
     {
         name: 'Sofi',
         logo: '/external-logos/sofi-logo.svg',
-        title: 'SoFi manages hundreds of microservices',
+        title: 'SoFi manages hundreds of microservices.',
         url: '/case-studies/sofi-moves-fast-on-hundreds-of-microservices',
     },
     {
@@ -86,7 +92,7 @@ const CASESTUDIES: CaseStudies[] = [
     {
         name: 'Thorn',
         logo: '/external-logos/thorn-logo.svg',
-        title: 'Thorn deprecates legacy code safely',
+        title: 'Thorn deprecates legacy code safely.',
         url: '/case-studies/we-are-thorn',
     },
 ]
